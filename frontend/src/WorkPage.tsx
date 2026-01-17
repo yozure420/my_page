c59500ce9f58dc5531a23f404e019010889e893f
yozure420 <hatune08mikumiku31@gmail.com>
2026-01-17 20:13:59 +0900

    commit

--- frontend/src/WorkPage.tsx
+++ frontend/src/WorkPage.tsx
@@ -1,9 +1,4 @@
 import { useState, useEffect } from 'react'
-// import reactLogo from './assets/react.svg'
-// import viteLogo from '/vite.svg'
-// import Header from './Header.tsx'
-// import Profile from './Profile.tsx'
-// import Contact from './Contact.tsx'
 import Work from './Work.tsx'
 
 
